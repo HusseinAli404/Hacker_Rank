813e963e9691d13315857bf34ef425fb2de3eca1
Hussein ali <73035164+hussein-ali-mc@users.noreply.github.com>
2021-07-18 07:59:56 +0200

    Add files via upload

---
+++ python/Python-Functions/map-and-lambda-functions.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com
+# https://github.com/hussein-ali-mc
+def get_fib(n):
+    array = []
+    for i in range(n):
+        if i == 0:
+            array.append(0)
+            continue
+        if i == 1:
+            array.append(1)
+            continue
+
+        array.append(array[i - 1] + array[i - 2])
+    return array
+
+
+N = int(input())
+
+fibonacci = get_fib(N)
+fibonacci = list(map(lambda x: pow(x, 3), fibonacci))
+print(fibonacci)
